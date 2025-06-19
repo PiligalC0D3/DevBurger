5d3e43a5a77cf153c0a38bd3c856678b816ecb05
Michel Silva <michelpiligal@gmail.com>
2025-06-19 00:56:22 -0300

    Projeto iniciado

---
+++ products.js
@@ -0,0 +1,9 @@
+const menuOption = [
+    { name: 'X-Salada', prince: 30, vegan: false, src: '.img/xsalada.jpeg'},
+    { name: 'X-Baco' , prince: 34, vegan: false, src: 'img/xbacon.png'},
+    { name: 'X-Baco Egg' , prince: 39, vegan: false, src: 'img/xbacon-egg.png'},
+    { name: 'Monstruoso' , prince: 50, vegan: false, src: 'img/monstruoso.png'},
+    { name: 'Big Vegano' , prince: 55, vegan: true, src: 'img/xvegan.png'},
+    { name: 'X-Vegan' , prince: 45, vegan: true, src: 'img/monstruoso-vegan.png'},
+]
+
