8a8db833d9ae4eddc32a02054a20953aa1f49a98
Michel Silva <michelpiligal@gmail.com>
2025-06-26 00:08:22 -0300

    função de desconto corrigida.

--- scripts.js
+++ scripts.js
@@ -10,10 +10,12 @@ btnFiltrar.addEventListener('click', filtrarVeganos);
 
 const menuList = document.querySelector('.menu');
 
-function renderMenu(products) {
+// Lista de produtos do menu//
+
+function renderMenu( productsArray ) {
   menuList.innerHTML = ''; // Limpa o menu
 
-  products.forEach(item => {
+  productsArray.forEach(item => {
     const li = document.createElement('li');
 
     li.innerHTML = `
@@ -26,22 +28,30 @@ function renderMenu(products) {
   });
 }
 
+// Função para mostrar o menu completo//
+
 function mostrarTudo() {
   menuList.classList.add('show'); // Adiciona a classe para mostrar o menu
   renderMenu(menuOptions); // Só mostra ao clicar no botão
 }
 
-function mapearNomes() {
-  const nomes = menuOptions.map(item => item.name);
+// Função para mapear os nomes e aplicar desconto//
+// e atualizar a lista de preços//
 
-  menuList.innerHTML = ''; // Limpa a lista
+function mapearNomes() { 
+  const novoPreco = menuOptions.map((product) => ({
+    ...product, // Mantém as outras propriedades do produto. Isso é um SPREAD OPERATOR//
+    price: product.price * 0.9,
+    //name: product.name.toUpperCase(), // Converte o nome para maiúsculas SEM O SPREAD OPERATOR//
+    //src: product.src // Mantém a imagem original SEM O SPREAD OPERATOR//
+    // Converte o nome para maiúsculas COM O SPREAD OPERATOR//
+  }));
 
-  nomes.forEach(nome => {
-    const li = document.createElement('li');
-    li.innerHTML = `<p>${nome}</p>`;
-    menuList.appendChild(li);
-  });
+  renderMenu(novoPreco);
 }
+
+// Função para somar todos os preços e exibir o total//
+
 
 function somarTudo() {
   const total = menuOptions.reduce((acc, item) => acc + item.price, 0);
@@ -50,6 +60,9 @@ function somarTudo() {
     <li><p>Total dos produtos: <strong>R$ ${total},00</strong></p></li>
   `;
 }
+
+// Função para filtrar os produtos veganos e renderizar a lista//
+// Exibe apenas os produtos veganos//
 
 function filtrarVeganos() {
   const veganos = menuOptions.filter(item => item.vegan);
